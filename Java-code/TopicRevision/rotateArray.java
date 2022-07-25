047cfe492a744f9d6c1f7d8b13ee5bb0e209a1e3
G.Hemanth Raaj <56654931+hemanth506@users.noreply.github.com>
2022-07-25 20:46:04 +0530

    LTR and RTL.

--- Java-code/TopicRevision/rotateArray.java
+++ Java-code/TopicRevision/rotateArray.java
@@ -14,33 +14,60 @@ public class rotateArray {
         return arr;
     }
 
-    ArrayList<Integer> rotate (ArrayList<Integer> arr, int times) {
+    ArrayList<Integer> rotateRTL (ArrayList<Integer> arr, int times) {
         int arrSize = arr.size();
         int f,e,k;
         f = 0;
         e = arrSize - 1;
         System.out.println("initial => " + arr);
         reverse(arr, f,e);
-        System.out.println(arr);
+        // System.out.println(arr);
 
         k = times % arrSize;
         f = 0;
         e = k - 1;
         reverse(arr, f,e);
-        System.out.println(arr);
+        // System.out.println(arr);
 
         f = k;
         e = arrSize - 1;
         reverse(arr, f,e);
-        System.out.println(arr);
+        System.out.println("RTL => " + arr);
+        return arr;
+    }
+
+    ArrayList<Integer> rotateLTR (ArrayList<Integer> arr, int times) {
+        int arrSize = arr.size();
+        int f,e,k;
+        f = 0;
+        e = arrSize - 1;
+        // System.out.println("initial => " + arr);
+        reverse(arr, f,e);
+        // System.out.println(arr);
+
+        k = times % arrSize;
+        f = 0;
+        e = arrSize - k - 1;
+        reverse(arr, f,e);
+        // System.out.println(arr);
+
+        f = arrSize - k;
+        e = arrSize - 1;
+        reverse(arr, f,e);
+        System.out.println("LTR => " + arr);
         return arr;
     }
     public static void main(String[] args) {
 
         rotateArray rotArr = new rotateArray();
         
-        int times = 7;
-        ArrayList<Integer> arr = new ArrayList<Integer>(Arrays.asList(3,-2,1,4,6,9,8));
-        rotArr.rotate(arr, times);
+        int times = 2;
+        ArrayList<Integer> arr2 = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9));
+        rotArr.rotateRTL(arr2, times);
+        
+        ArrayList<Integer> arr = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9));
+        rotArr.rotateLTR(arr, times);
+
+
     }
 }
