b819c966d5d5411ab1a2b0ef28ea8f07d3dfd7bf
G.Hemanth Raaj <56654931+hemanth506@users.noreply.github.com>
2022-08-05 11:44:14 +0530

    removed unused declaration.

--- Java-code/Introduction/Demo.java
+++ Java-code/Introduction/Demo.java
@@ -5,11 +5,11 @@ import java.util.Arrays;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
+// import java.io.InputStreamReader;
+// import java.io.BufferedReader;
 import java.io.IOException;
 
-import java.io.Console;
+// import java.io.Console;
 public class Demo{
 
     void dataTypes() {
