6363837dba91cb0e1a4fa8b46a8948a3dd71fc13
G.Hemanth Raaj <56654931+hemanth506@users.noreply.github.com>
2022-08-21 23:45:11 +0530

    basic check

---
+++ Java-code/TopicRevision/modulusCheck.java
@@ -0,0 +1,8 @@
+public class modulusCheck {
+    public static void main(String[] args) {
+        System.out.println((-40 % 7) + 7);
+        System.out.println((-60 % 9) + 9);
+        System.out.println((-40 % 9) + 9);
+
+    }
+}
