4902998b6abf61c94fda73a2505ddf009501a4e0
G.Hemanth Raaj <56654931+hemanth506@users.noreply.github.com>
2022-08-24 12:24:50 +0530

    minor change

--- Java-code/Scaler/live-session/practise-works/contest2/littlePonnyAndPalindromes.java
+++ Java-code/Scaler/live-session/practise-works/contest2/littlePonnyAndPalindromes.java
@@ -3,19 +3,19 @@ import java.util.HashMap;
 public class littlePonnyAndPalindromes {
     public int solve(String A) {
         int N = A.length();
-        HashMap<Character, Integer> hs = new HashMap<>();
+        HashMap<Character, Integer> hm = new HashMap<>();
         for (int i = 0; i < N; i++) {
-            if (hs.containsKey(A.charAt(i))) {
-                int value = hs.get(A.charAt(i)) + 1;
-                hs.put(A.charAt(i), value);
+            if (hm.containsKey(A.charAt(i))) {
+                int value = hm.get(A.charAt(i)) + 1;
+                hm.put(A.charAt(i), value);
             } else {
-                hs.put(A.charAt(i), 1);
+                hm.put(A.charAt(i), 1);
             }
         }
-        System.out.println(hs);
+        System.out.println(hm);
 
         int countOdd = 0;
-        for (HashMap.Entry<Character, Integer> m : hs.entrySet()) {
+        for (HashMap.Entry<Character, Integer> m : hm.entrySet()) {
             if(m.getValue() % 2 != 0) {
                 countOdd += 1;
             }
