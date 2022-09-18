ca4b652d31de2a0934ec0b6091130367d00f2e09
G.Hemanth Raaj <56654931+hemanth506@users.noreply.github.com>
2022-09-18 23:30:56 +0530

    Mid - count the number of least value char on the right and mutiply with the factorial of i-1 and return the sum.

---
+++ Java-code/Scaler-advanced/live-session/Homeworks/40 - Permutation & Combination/sortedPermutationRank.java
@@ -0,0 +1,43 @@
+
+public class sortedPermutationRank {
+    public int findRank(String A) {
+        int N = A.length();
+        int mod = 1000003;
+        int rank = 0;
+        for(int i = 0; i < N; i++) {
+            System.out.println("====" + i + "====");
+            char curChar = A.charAt(i);
+            int ctChar = 0;
+            for(int j = i+1; j < N; j++) {
+                if(A.charAt(j) < curChar) {
+                    System.out.println(A.charAt(j));
+                    ctChar++;
+                }
+            }
+
+            if(ctChar != 0) {
+                System.out.println(N-i);
+                int sumV = 1;
+                for(int k = 1; k <= (N-i-1); k++) {
+                    sumV = (sumV%mod * k%mod)%mod;
+                }
+                System.out.println("sumV " + sumV);
+                rank += (sumV%mod * ctChar%mod)%mod;
+            }
+            System.out.println("ctChar " + ctChar);
+            System.out.println("=======================");
+        }
+        rank+=1;
+        System.out.println(rank);
+        return rank%mod;
+    }
+    public static void main(String[] args) {
+        sortedPermutationRank spr = new sortedPermutationRank();
+        // String A = "fgeih";
+        // String A = "acb";
+        // String A = "a";
+        String A = "VIEW";
+
+        spr.findRank(A);
+    }
+}
