554d08d91e48eca41febf0f8cd24f6fc2f5f916f
G.Hemanth Raaj <56654931+hemanth506@users.noreply.github.com>
2022-10-02 22:38:49 +0530

    Added another approach.

--- Java-code/Scaler-advanced/live-session/Homeworks/32 - Arrays - 1/maxNonNegativeSubArray.java
+++ Java-code/Scaler-advanced/live-session/Homeworks/32 - Arrays - 1/maxNonNegativeSubArray.java
@@ -2,11 +2,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class maxNonNegativeSubArray {
-    public ArrayList<Integer> maxset(ArrayList<Integer> A) {
+    // Using Split and iterate tech - TC = O(N) SC = O(Range)
+    public ArrayList<Integer> maxsetOLD(ArrayList<Integer> A) {
         int N = A.size();
         System.out.println(A);
         System.out.println("===================================");
-        
+
         // split to the left when you find the negative value and find the sum
         long total;
         int arrLength;
@@ -14,36 +15,34 @@ public class maxNonNegativeSubArray {
         int maxArrLength = 0;
         int startingIndex = -1;
         int endingIndex = -1;
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i < N; i++) {
             if (A.get(i) < 0) {
-                if (i != 0) {
-                    total = 0;
-                    arrLength = 0;
-                    int j;
-                    innerLoop: for (j = i - 1; j >= 0; j--) {
-                        if (A.get(j) < 0) {
-                            break innerLoop;
-                        }
-                        total += A.get(j);
-                        arrLength += 1;
+                total = 0;
+                arrLength = 0;
+                int j;
+                innerLoop: for (j = i - 1; j >= 0; j--) {
+                    if (A.get(j) < 0) {
+                        break innerLoop;
                     }
+                    total += A.get(j);
+                    arrLength += 1;
+                }
 
-                    if (total > maxTotal) {
-                        maxTotal = total;
+                if (total > maxTotal) {
+                    maxTotal = total;
+                    maxArrLength = arrLength;
+                    startingIndex = j + 1;
+                    endingIndex = i - 1;
+                } else if (total == maxTotal) {
+                    maxTotal = total;
+                    if (arrLength > maxArrLength) {
                         maxArrLength = arrLength;
                         startingIndex = j + 1;
                         endingIndex = i - 1;
-                    } else if (total == maxTotal) {
-                        maxTotal = total;
-                        if (arrLength > maxArrLength) {
-                            maxArrLength = arrLength;
-                            startingIndex = j + 1;
-                            endingIndex = i - 1;
-                        }
                     }
-                    // System.out.println("total => " + total + " " + maxTotal);
-                    // System.out.println("length => " + arrLength + " " + maxArrLength);
                 }
+                // System.out.println("total => " + total + " " + maxTotal);
+                // System.out.println("length => " + arrLength + " " + maxArrLength);
                 // System.out.println("===================================");
             }
         }
@@ -85,15 +84,76 @@ public class maxNonNegativeSubArray {
         return subArray;
     }
 
+    // Using prefixSum - TC = O(N) SC = O(N + Range)
+    public ArrayList<Integer> maxset(ArrayList<Integer> A) {
+        int N = A.size();
+        ArrayList<Long> B = new ArrayList<Long>();
+        int ctNeg = 0;
+        for(int i = 0; i < N; i++) {
+            B.add((long)A.get(i));
+            if (A.get(i) < 0) {
+                ctNeg += 1;
+            }
+        }
+        if (ctNeg == N) {
+            return new ArrayList<Integer>();
+        }
+
+        long sumV = 0;
+        for (int i = 1; i < N; i++) {
+            if (B.get(i) > 0) {
+                sumV = B.get(i);
+                if (B.get(i - 1) > 0) {
+                    sumV += B.get(i - 1);
+                }
+                B.set(i, sumV);
+            }
+        }
+        B.add(1L*-1);
+        System.out.println(B);
+        // System.out.println(startInd + " " + endInd);
+        N = B.size();
+        int endInd = 0;
+        long maxV = Long.MIN_VALUE;
+        for (int i = 1; i < N; i++) {
+            if (B.get(i) < 0) {
+                System.out.println(B.get(i - 1));
+                if (B.get(i - 1) > maxV) {
+                    endInd = i-1;
+                    maxV = B.get(i-1);
+                }
+            }
+        }
+        
+        int startInd = 0;
+        for(int i = 0; i < endInd; i++) {
+            if(B.get(i) < 0) {
+                startInd = i + 1;
+            }
+        }
+        System.out.println("startInd = " + startInd + " endInd = " + endInd);
+
+        ArrayList<Integer> C = new ArrayList<Integer>();
+        for(int i = startInd; i <= endInd; i++) {
+            C.add(A.get(i));
+        }
+        System.out.println(C);
+        return C;
+    }
+
     public static void main(String[] args) {
         maxNonNegativeSubArray mn = new maxNonNegativeSubArray();
 
         // ArrayList<Integer> A = new ArrayList<Integer>(Arrays.asList(1, 2, 5, -7, 2, 3, -1, 1, -3, -2, 1, 4, 1, 1, 1, -5, 1, 1, 4, 1, 1));
 
-        // ArrayList<Integer> A = new ArrayList<Integer>(Arrays.asList(10, -1, 2, 3, -4,
-        // 100));
+        // ArrayList<Integer> A = new ArrayList<Integer>(Arrays.asList(1, 2, 5, -7, 2,3));
 
-        ArrayList<Integer> A = new ArrayList<Integer>(Arrays.asList(1967513926, 1540383426, -1303455736, -521595368));
+        // ArrayList<Integer> A = new ArrayList<Integer>(Arrays.asList(10, -1, 2, 3, -4, 100));
+
+        ArrayList<Integer> A = new ArrayList<Integer>(Arrays.asList(1967513926,1540383426, -1303455736, -521595368));
+
+        // ArrayList<Integer> A = new ArrayList<Integer>(Arrays.asList(-1, -1, -1, -1, -1));
+
         mn.maxset(A);
     }
 }
