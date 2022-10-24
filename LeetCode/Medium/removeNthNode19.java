451d956e994d4fa89fcfb8552ebd44032bd21e7e
G.Hemanth Raaj <56654931+hemanth506@users.noreply.github.com>
2022-10-24 11:11:20 +0530

    Mid - Problem solving with LinkedList..

---
+++ LeetCode/Medium/removeNthNode19.java
@@ -0,0 +1,36 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class removeNthNode19 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        int ct = 0;
+        ListNode h = head;
+        while(h != null) {
+            ct += 1;
+            h = h.next;
+        }
+
+        int removeIt = ct - n + 1;
+        ListNode th = head; 
+        for(int i = 1; i < removeIt; i++) {
+            th = th.next;
+        }
+
+        if(th.next != null && th.next.next != null) {
+            th.next = th.next.next;
+        } else {
+            th.next = null;
+        }
+        return head;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
