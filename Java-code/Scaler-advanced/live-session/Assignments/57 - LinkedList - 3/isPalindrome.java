3d764943aba2368e6592a74acb7ddc811a114789
G.Hemanth Raaj <56654931+hemanth506@users.noreply.github.com>
2022-10-31 13:14:43 +0530

    Mid: reverse from the mid and check if it equal.

--- Java-code/Scaler-advanced/live-session/Assignments/57 - LinkedList - 3/isPalindrome.java
+++ Java-code/Scaler-advanced/live-session/Assignments/57 - LinkedList - 3/isPalindrome.java
@@ -10,7 +10,8 @@ class ListNode {
 }
 
 public class isPalindrome {
-    public int lPalin(ListNode A) {
+    // with extra space
+    public int lPalinWithSpace(ListNode A) {
         ListNode slow = A;
         ListNode fast = A;
 
@@ -57,6 +58,54 @@ public class isPalindrome {
             i += 1;
         }
 
+        return 1;
+    }
+
+    // with no extra space
+    public ListNode midNode(ListNode A) {
+        ListNode slow = A;
+        ListNode fast = A;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public ListNode reverseNode(ListNode A) {
+        ListNode tempA = A;
+        ListNode rHead = null;
+        ListNode helper = null;
+        while (tempA != null) {
+            helper = tempA;
+            tempA = tempA.next;
+            if(rHead == null) {
+                helper.next = null;
+                rHead = helper;
+            } else {
+                helper.next = rHead;
+                rHead = helper;
+            }
+        } 
+        return rHead;
+    }
+
+    public int lPalin(ListNode A) {
+        ListNode firstHalf = A;
+        ListNode midNode = midNode(A);
+        ListNode secondHalf = midNode.next;
+        midNode.next = null;
+
+        ListNode reversedNode = reverseNode(secondHalf);
+        ListNode rNode = reversedNode;
+        while(rNode != null) {
+            if(firstHalf.val != rNode.val) {
+                return 0;
+            } 
+            rNode = rNode.next;
+            firstHalf = firstHalf.next;
+        }
         return 1;
     }
 }
