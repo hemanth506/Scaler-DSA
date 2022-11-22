b2fd1ecae1754fe37ae2af9c39e244349df57be4
G.Hemanth Raaj <56654931+hemanth506@users.noreply.github.com>
2022-11-22 12:48:44 +0530

    Aded tree class.

--- Java-code/Scaler-advanced/live-session/Homeworks/65 - Trees - 4 - LCA/distanceBetweenTwoNodes.java
+++ Java-code/Scaler-advanced/live-session/Homeworks/65 - Trees - 4 - LCA/distanceBetweenTwoNodes.java
@@ -1,5 +1,15 @@
 import java.util.ArrayList;
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
 
+    TreeNode(int x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+}
 public class distanceBetweenTwoNodes {
     public boolean path(TreeNode A, int B, ArrayList<Integer> arrB) {
         if(A == null) {
