7f410b8fa8ea248e5dbf43b6ce1a2e15d56cd29c
G.Hemanth Raaj <56654931+hemanth506@users.noreply.github.com>
2022-11-22 12:46:42 +0530

    Mid: first find the path and then use it to find the distance.

---
+++ Java-code/Scaler-advanced/live-session/Homeworks/65 - Trees - 4 - LCA/distanceBetweenTwoNodes.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+
+public class distanceBetweenTwoNodes {
+    public boolean path(TreeNode A, int B, ArrayList<Integer> arrB) {
+        if(A == null) {
+            return false;
+        }
+
+        if(A.val == B) {
+            arrB.add(A.val);
+            return true;
+        }
+
+        if(path(A.left, B, arrB) || path(A.right, B, arrB)) {
+            arrB.add(A.val);
+            return true;
+        }
+        return false;
+    }
+    public int solve(TreeNode A, int B, int C) {
+        ArrayList<Integer> pathA = new ArrayList<Integer>();
+        path(A, B, pathA);
+
+
+        ArrayList<Integer> pathB = new ArrayList<Integer>();
+        path(A, C, pathB);
+
+        int N = pathA.size() > pathB.size() ? pathB.size() : pathA.size();
+        // System.out.println(pathA);
+        // System.out.println(pathB);
+
+        int iA = pathA.size()-1;
+        int iB = pathB.size()-1;
+        // System.out.println(pathA.size() + " size " + pathB.size());
+
+        int ct = 1;
+        int tempA = iA;
+        int tempB = iB;
+
+        while(ct <= N) {
+            int curValA = pathA.get(iA);
+            int curValB = pathB.get(iB);
+
+            if(curValA == curValB) {
+                tempA = iA;
+                tempB = iB;
+            }
+            ct++;
+            iA--;
+            iB--;
+        }
+        System.out.println(tempA + tempB - 1);
+        return C;
+    }
+}
