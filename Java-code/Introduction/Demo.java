916b1fbb78beb5b95cf12add6f2f36f3cb19ceab
G.Hemanth Raaj <hgirirajan@teamhgs.com>
2023-07-06 14:31:35 +0530

    Basics

--- Java-code/Introduction/Demo.java
+++ Java-code/Introduction/Demo.java
@@ -1,6 +1,6 @@
 
-
 import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -10,11 +10,11 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 // import java.io.Console;
-public class Demo{
+public class Demo {
 
     void dataTypes() {
         short b = 20000;
-        System.out.println("byte value is "+ b);
+        System.out.println("byte value is " + b);
 
         boolean soldOut = true;
         System.out.println(soldOut);
@@ -30,13 +30,13 @@ public class Demo{
         // Declar int arr of size 50
         int arr[] = new int[50];
         int i, j;
-        for(i = 1, j = 2; i <= 50; i++, j+=2){
+        for (i = 1, j = 2; i <= 50; i++, j += 2) {
             arr[i - 1] = j;
         }
         System.out.println(Arrays.toString(arr));
 
         // 2D array amtrix
-        int[][] newArr = new int[3][4]; 
+        int[][] newArr = new int[3][4];
         System.out.println(newArr);
     }
 
@@ -58,12 +58,10 @@ public class Demo{
 
         int limit = scan.nextInt();
         int sumValue = 0;
-        outerLoop:
-        for (int i = 0; i <= limit; i++) {
-            innerLoop:
-            for (int j = 0; j <= limit; j++) {
-                if (i == 3){
-                    if(j % 4 == 0) {
+        outerLoop: for (int i = 0; i <= limit; i++) {
+            innerLoop: for (int j = 0; j <= limit; j++) {
+                if (i == 3) {
+                    if (j % 4 == 0) {
                         System.out.println("Broke from outerloop");
                         break outerLoop;
                     } else if (j % 3 == 0) {
@@ -71,7 +69,7 @@ public class Demo{
                         break innerLoop;
                     }
                 }
-                sumValue += i*j;
+                sumValue += i * j;
             }
         }
 
@@ -79,7 +77,7 @@ public class Demo{
     }
 
     void switchCase(Scanner scan) {
-        // 
+        //
         System.out.println("Enter a number from 1 to 7");
         int integer = scan.nextInt();
 
@@ -109,7 +107,7 @@ public class Demo{
                 break;
         }
 
-        // 
+        //
         System.out.println("Enter any alphabet ");
         int alphabet = scan.next().charAt(0);
 
@@ -122,7 +120,8 @@ public class Demo{
                 System.out.println("Vowel");
                 break;
             default:
-                System.out.println("Consonant");;
+                System.out.println("Consonant");
+                ;
         }
 
         // Nested Switch case.
@@ -135,21 +134,24 @@ public class Demo{
         System.out.println("Enter dishes: ");
         String dishes = scan.next();
 
-        switch(courseCategory) {
+        switch (courseCategory) {
             case "appetizer":
                 System.out.println("You selected appetizer");
-                System.out.println("Your brunch: " + "Pita bread sticks with hummus and crystal lemonade "+ " will be ready to serve in 9 minutes");
+                System.out.println("Your brunch: " + "Pita bread sticks with hummus and crystal lemonade "
+                        + " will be ready to serve in 9 minutes");
                 break;
 
             case "maincourse":
                 System.out.println("You selected main course");
-                switch(course) {
+                switch (course) {
                     case "brunch":
-                        System.out.println("Your brunch: Crispy Baked Chicken Wings with mashed potatoes and ice tea  will be ready to serve in 9 minutes");
+                        System.out.println(
+                                "Your brunch: Crispy Baked Chicken Wings with mashed potatoes and ice tea  will be ready to serve in 9 minutes");
                         break;
                     case "lunch":
-                        System.out.println("Your lunch: roasted chicken panini with coleslaw and mojito will be ready to serve in 12 minutes");
-                        switch(dishes) {
+                        System.out.println(
+                                "Your lunch: roasted chicken panini with coleslaw and mojito will be ready to serve in 12 minutes");
+                        switch (dishes) {
                             case "fish":
                                 System.out.println("Fishes are good");
                                 break;
@@ -162,7 +164,8 @@ public class Demo{
                         }
                         break;
                     case "dinner":
-                        System.out.println("Your dinner: cheese chicken ravioli with garlic bread and mojito will be ready to serve in 13 minutes");
+                        System.out.println(
+                                "Your dinner: cheese chicken ravioli with garlic bread and mojito will be ready to serve in 13 minutes");
                         break;
                     default:
                         System.out.println("Wrong input in inner switch");
@@ -170,16 +173,17 @@ public class Demo{
                 }
                 break;
             case "desert":
-                System.out.println("Your snack: Blueberry cheesecake with chocolate fudge syrup will be ready to serve in 7 minutes");
+                System.out.println(
+                        "Your snack: Blueberry cheesecake with chocolate fudge syrup will be ready to serve in 7 minutes");
                 break;
             default:
                 System.out.println("Wrong input");
         }
 
     }
-    
+
     void getUserInput(Scanner scan) {
-        
+
         System.out.println("Enter your name ?");
         String name = scan.nextLine();
 
@@ -193,48 +197,47 @@ public class Demo{
         System.out.println("Enter string? ");
         String name2 = scan.nextLine();
 
-        System.out.println("Your name is " + name + " and your rating is "+ rating + " and the short value is "+ var + name2);
-        
+        System.out.println(
+                "Your name is " + name + " and your rating is " + rating + " and the short value is " + var + name2);
+
     }
-    
-    void pyramid(Scanner scan){
+
+    void pyramid(Scanner scan) {
         int pyramidValue = scan.nextInt();
-        for(int i = 0; i < pyramidValue; i++) {
-            for(int j = 0; j < pyramidValue; j++) {
-                if(j < pyramidValue - i - 1){
+        for (int i = 0; i < pyramidValue; i++) {
+            for (int j = 0; j < pyramidValue; j++) {
+                if (j < pyramidValue - i - 1) {
                     System.out.print(" ");
                 } else {
                     System.out.print("#");
                 }
-            } 
+            }
             System.out.println();
         }
     }
-    
+
     int leetCodeMissingNumbers(int[] nums) {
         int n = nums.length;
         int sum = 0;
-        for(int i = 0; i< n; i++) {
+        for (int i = 0; i < n; i++) {
             sum += nums[i];
         }
-        System.out.print(n * (n+1)/2 - sum);
-        return n * (n+1)/2 - sum;
+        System.out.print(n * (n + 1) / 2 - sum);
+        return n * (n + 1) / 2 - sum;
     }
-    
 
     void getUserInputFiles() {
         try {
             File newFile = new File("sample.txt");
             Scanner obj = new Scanner(newFile);
-    
+
             while (obj.hasNextLine()) {
                 String data = obj.nextLine();
                 System.out.println(data);
             }
             obj.close();
 
-        } 
-        catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
@@ -246,43 +249,43 @@ public class Demo{
         Coconutrum,
         Gin,
     }
-    
+
     public void enumSwitchCase() {
         DrinkMenu order = DrinkMenu.Martini;
         switch (order) {
             case Gin:
-              System.out.println("Wait time for Gin with tonic water is 3 mins");
-              break;
+                System.out.println("Wait time for Gin with tonic water is 3 mins");
+                break;
             case Coconutrum:
-              System.out.println("Wait time for Coconut rum is 2 mins");
-              break;
+                System.out.println("Wait time for Coconut rum is 2 mins");
+                break;
             case Martini:
-              System.out.println("Wait time for Crushed Martini is 3 mins");
-              break;
+                System.out.println("Wait time for Crushed Martini is 3 mins");
+                break;
             case Liit:
-              System.out.println("Wait time for Long island iced tea is 6 mins");
-              break;
-          }
+                System.out.println("Wait time for Long island iced tea is 6 mins");
+                break;
+        }
     }
-    
+
     public static void exitCondition() {
         for (int i = 0; i < 10; i++) {
-            if (i > 4) {                   
+            if (i > 4) {
                 System.out.println("Terminating JVM...");
                 System.exit(0);
             }
-            System.out.println("Array Index: " + i );
+            System.out.println("Array Index: " + i);
         }
         System.out.println("I will not be printed because the JVM is terminated..");
     }
-    
-    void maxMinOFArr () {
-        int[] arr = {1, 2,0, 3, 4, 5};
+
+    void maxMinOFArr() {
+        int[] arr = { 1, 2, 0, 3, 4, 5 };
 
         int maxValue = Integer.MIN_VALUE;
         int minValue = Integer.MAX_VALUE;
 
-        for(int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             if (maxValue < arr[i]) {
                 maxValue = arr[i];
             }
@@ -293,7 +296,7 @@ public class Demo{
         }
         System.out.println(minValue + " " + maxValue);
     }
-    
+
     public static void main(String[] args) throws IOException {
         System.out.println("Congrats Hemanth for starting Java");
 
@@ -323,7 +326,6 @@ public class Demo{
         // String name = reader.readLine();
         // System.out.println("You entered: ");
         // System.out.println(name);
-        
 
         // Console c = System.console();
         // System.out.println("Expected Salary?");
@@ -333,16 +335,22 @@ public class Demo{
         // char[] pass = c.readPassword();
         // System.out.println(pass);
 
-        
         // newDemo.enumSwitchCase();
 
         // newDemo.exitCondition();
 
         // newDemo.maxMinOFArr();
 
-        booleanXORComp();
+        // booleanXORComp();
 
+        assignment();
         scan.close();
+    }
+
+    private static void assignment() {
+        int[] numbers = { 1, 5, 4, 6, 7, 8, 9, 10, 11 };
+
+        System.out.println(Arrays.binarySearch(numbers, 4));
     }
 
     private static void booleanXORComp() {
