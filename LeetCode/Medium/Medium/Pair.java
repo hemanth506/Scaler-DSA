27ecdeee6e47768bed5ad75587ad505350afa260
G.Hemanth Raaj <hgirirajan@teamhgs.com>
2023-07-11 13:40:58 +0530

    Added package.

---
+++ LeetCode/Medium/Medium/Pair.java
@@ -0,0 +1,10 @@
+package Medium;
+
+public class Pair {
+    public Integer key;
+    public Integer value;
+    public Pair(Integer x, Integer y) {
+        this.key = x;
+        this.value = y;
+    }
+}
