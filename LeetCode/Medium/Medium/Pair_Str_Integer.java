200a4d4b640ff00a9d452a63ef0ef87e278ef83c
G.Hemanth Raaj <hemanthraaj31@gmail.com>
2023-07-20 21:59:59 +0530

    Class declaration

---
+++ LeetCode/Medium/Medium/Pair_Str_Integer.java
@@ -0,0 +1,10 @@
+package Medium;
+
+public class Pair_Str_Integer {
+    public String word;
+    public Integer freq;
+    public Pair_Str_Integer(Integer x, String y) {
+        this.freq = x;
+        this.word = y;
+    }
+}
