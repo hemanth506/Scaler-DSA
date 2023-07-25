b590bf884a177428163c0d46a15c10eec246eb2e
G.Hemanth Raaj <hemanthraaj31@gmail.com>
2023-07-25 10:04:38 +0530

    Added Pair

---
+++ LeetCode/Medium/Medium/Pair_Int_Str.java
@@ -0,0 +1,11 @@
+package Medium;
+
+public class Pair_Int_Str {
+    public Integer views;
+    public String ids;
+
+    public Pair_Int_Str(Integer x, String y) {
+        this.views = x;
+        this.ids = y;
+    }
+}
