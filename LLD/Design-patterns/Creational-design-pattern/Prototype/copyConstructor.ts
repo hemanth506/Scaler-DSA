68680afd5f7d5e2a26d620976ff07a1e3c3928e2
HemanthRaajG <hemanthraaj31@gmail.com>
2025-12-12 02:40:10 +0530

    Prototype Design pattern

--- LLD/Design-patterns/Creational-design-pattern/Prototype/copyConstructor.ts
+++ LLD/Design-patterns/Creational-design-pattern/Prototype/copyConstructor.ts
@@ -1,51 +1,50 @@
-class Student {
-    name?: string;
-    age?: number;
-    batch?: number;
-    psp?: number;
-    univName?: string;
+interface Prototype<T> {
+    clone(): T;
+}
 
-    constructor(name?: string, age?: number, batch?: number, psp?: number, univName?: string) {
-        this.age = age
+class Student implements Prototype<Student>{
+    name: string;
+    age: number;
+    batch: number;
+    psp: number;
+    univName: string;
+
+    constructor(name: string, age: number, batch: number, psp: number, univName: string) {
         this.name = name
-        this.univName = univName;
+        this.age = age
         this.batch = batch
         this.psp = psp
+        this.univName = univName;
     }
 
-    public static copyConstructor(sd: Student) {
-        return new Student(sd.name, sd.age, sd.batch, sd.psp, sd.univName)
+    public clone(): Student {
+        return new Student(this.name, this.age, this.batch, this.psp, this.univName)
     }
 }
 
-let sd = new Student("Hemanth", 27, 2019, 63.5, "Anna University")
-let sd2 = Student.copyConstructor(sd)
-console.log(sd)
-console.log(sd2)
-console.log(sd === sd2)
+class BrightStudent extends Student implements Prototype<BrightStudent> {
+    iq: number
 
-
-class BrightStudent extends Student {
-    iq?: number
-    
-    constructor(iq?: number) {
-        super()
+    constructor(iq: number, name: string, age: number, batch: number, psp: number, univName: string) {
+        super(name, age, batch, psp, univName)
         this.iq = iq
     }
+
+    public clone(): BrightStudent {
+        return new BrightStudent(this.iq, this.name, this.age, this.batch, this.psp, this.univName)
+    }
 }
 
-
-const getRandomClass = ():  BrightStudent | Student => {
+const getRandomClass = (): BrightStudent | Student => {
     const randomValue = Math.floor(Math.random() * 10) + 1;
 
-    if(randomValue % 2 === 0) {
-        return new BrightStudent(23)
-    } 
-    return new Student()
+    if (randomValue % 2 === 0) {
+        return new BrightStudent(99, "Ruchika", 27, 2020, 68.5, "Anna University")
+    }
+    return new Student("Hemanth", 28, 2019, 63.5, "Anna University")
 }
 
-const currentClass: BrightStudent = getRandomClass();
-if(currentClass instanceof BrightStudent) {
-    console.log('🚀 ~ currentClass:', currentClass.iq)
-}
+const currentClass: BrightStudent | Student = getRandomClass();
+const newObj = currentClass.clone()
+console.log('🚀 ~ newObj:', newObj, ' \n currentClass', currentClass)
 
